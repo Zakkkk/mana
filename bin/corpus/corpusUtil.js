dc68eeb4f77c8f806906991fe1a6121e3c09b09e
Zakkkk <zak.g.venter@gmail.com>
2025-01-29 01:53:51 +1100

    Cleaned up magic replacement

--- bin/corpus/corpusUtil.js
+++ bin/corpus/corpusUtil.js
@@ -11,19 +11,19 @@ const mismatchingLetters = (stringToCheck, allowedLetters) => [...stringToCheck]
 exports.mismatchingLetters = mismatchingLetters;
 const getMonograms = (corpus, layout) => {
     const monograms = {};
-    if (!layout.hasMagic) {
-        // figure this out later
-        // return monograms;
-    }
     for (let extendedMonogram in corpus.extendedMonograms) {
         const freq = corpus.extendedMonograms[extendedMonogram];
-        if (extendedMonogram.length == 2) {
-            layout.magicRules.forEach((magicRule) => {
-                if (magicRule[0] == extendedMonogram[0] &&
-                    magicRule[1] == extendedMonogram[1])
-                    extendedMonogram = layout.magicIdentifier;
-            });
-        }
+        let magicReplacements = 0;
+        if (layout.hasMagic)
+            for (let i = 0; i < layout.magicRules.length; i++) {
+                const replacement = extendedMonogram.replace(layout.magicRules[i], layout.magicRules[i][0] + layout.magicIdentifier);
+                if (replacement != extendedMonogram) {
+                    extendedMonogram = replacement;
+                    magicReplacements++;
+                    if (magicReplacements == 1)
+                        break;
+                }
+            }
         if (extendedMonogram.length == 2)
             extendedMonogram = extendedMonogram[1];
         addGramAmount(extendedMonogram, freq, monograms);
@@ -38,29 +38,19 @@ const getMonograms = (corpus, layout) => {
 exports.getMonograms = getMonograms;
 const getBigrams = (corpus, layout) => {
     const bigrams = {};
-    if (!layout.hasMagic) {
-        // figure this out later
-        // return bigrams;
-    }
     for (let extendedBigram in corpus.extendedBigrams) {
         const freq = corpus.extendedBigrams[extendedBigram];
-        if (extendedBigram.length == 2) {
-            layout.magicRules.forEach((magicRule) => {
-                if (magicRule[0] == extendedBigram[0] &&
-                    magicRule[1] == extendedBigram[1])
-                    extendedBigram = extendedBigram[0] + layout.magicIdentifier;
-            });
-        }
-        else if (extendedBigram.length == 3) {
-            layout.magicRules.forEach((magicRule) => {
-                if (magicRule[0] == extendedBigram[0] &&
-                    magicRule[1] == extendedBigram[1])
-                    extendedBigram = layout.magicIdentifier + extendedBigram[2];
-                else if (magicRule[0] == extendedBigram[1] &&
-                    magicRule[1] == extendedBigram[2])
-                    extendedBigram = extendedBigram[1] + layout.magicIdentifier;
-            });
-        }
+        let magicReplacements = 0;
+        if (layout.hasMagic)
+            for (let i = 0; i < layout.magicRules.length; i++) {
+                const replacement = extendedBigram.replace(layout.magicRules[i], layout.magicRules[i][0] + layout.magicIdentifier);
+                if (replacement != extendedBigram) {
+                    extendedBigram = replacement;
+                    magicReplacements++;
+                    if (magicReplacements == 1)
+                        break;
+                }
+            }
         if (extendedBigram.length == 3)
             extendedBigram = extendedBigram.slice(1, 3);
         addGramAmount(extendedBigram, freq, bigrams);
@@ -81,49 +71,19 @@ const getBigrams = (corpus, layout) => {
 exports.getBigrams = getBigrams;
 const getTrigrams = (corpus, layout) => {
     const trigrams = {};
-    if (!layout.hasMagic) {
-        // figure this out later
-        // return trigrams;
-    }
     for (let extendedTrigram in corpus.extendedTrigrams) {
         const freq = corpus.extendedTrigrams[extendedTrigram];
-        if (extendedTrigram.length == 3) {
-            layout.magicRules.forEach((magicRule) => {
-                if (magicRule[0] == extendedTrigram[0] &&
-                    magicRule[1] == extendedTrigram[1])
-                    extendedTrigram =
-                        extendedTrigram[0] + layout.magicIdentifier + extendedTrigram[2];
-                else if (magicRule[0] == extendedTrigram[1] &&
-                    magicRule[1] == extendedTrigram[2])
-                    extendedTrigram =
-                        extendedTrigram[0] + extendedTrigram[1] + layout.magicIdentifier;
-            });
-        }
-        else if (extendedTrigram.length == 4) {
-            layout.magicRules.forEach((magicRule) => {
-                if (magicRule[0] == extendedTrigram[0] &&
-                    magicRule[1] == extendedTrigram[1])
-                    extendedTrigram =
-                        extendedTrigram[0] +
-                            layout.magicIdentifier +
-                            extendedTrigram[2] +
-                            extendedTrigram[3];
-                if (magicRule[0] == extendedTrigram[1] &&
-                    magicRule[1] == extendedTrigram[2])
-                    extendedTrigram =
-                        extendedTrigram[0] +
-                            extendedTrigram[1] +
-                            layout.magicIdentifier +
-                            extendedTrigram[3];
-                if (magicRule[0] == extendedTrigram[2] &&
-                    magicRule[1] == extendedTrigram[3])
-                    extendedTrigram =
-                        extendedTrigram[0] +
-                            extendedTrigram[1] +
-                            extendedTrigram[2] +
-                            layout.magicIdentifier;
-            });
-        }
+        let magicReplacements = 0;
+        if (layout.hasMagic)
+            for (let i = 0; i < layout.magicRules.length; i++) {
+                const replacement = extendedTrigram.replace(layout.magicRules[i], layout.magicRules[i][0] + layout.magicIdentifier);
+                if (replacement != extendedTrigram) {
+                    extendedTrigram = replacement;
+                    magicReplacements++;
+                    if (magicReplacements == 2)
+                        break;
+                }
+            }
         if (extendedTrigram.length == 4)
             extendedTrigram = extendedTrigram.slice(1, 4);
         addGramAmount(extendedTrigram, freq, trigrams);
@@ -144,33 +104,20 @@ const getTrigrams = (corpus, layout) => {
 exports.getTrigrams = getTrigrams;
 const getSkip2grams = (corpus, layout) => {
     const skip2grams = {};
-    if (!layout.hasMagic) {
-        // figure this out later
-        // return skip2grams;
-    }
     for (let extendedSkip2gram in corpus.extendedSkip2grams) {
         const freq = corpus.extendedSkip2grams[extendedSkip2gram];
         let parts = [extendedSkip2gram[0], extendedSkip2gram[1]];
-        if (extendedSkip2gram.length == 3) {
-            layout.magicRules.forEach((magicRule) => {
-                if (magicRule[0] == extendedSkip2gram[1] &&
-                    magicRule[1] == extendedSkip2gram[2])
-                    parts = [extendedSkip2gram[0], layout.magicIdentifier];
-                else
-                    parts = [extendedSkip2gram[0], extendedSkip2gram[2]];
-            });
-        }
-        else if (extendedSkip2gram.length == 4) {
-            parts = [extendedSkip2gram[1], extendedSkip2gram[3]];
-            layout.magicRules.forEach((magicRule) => {
-                if (magicRule[0] == extendedSkip2gram[0] &&
-                    magicRule[1] == extendedSkip2gram[1])
-                    parts[0] = layout.magicIdentifier;
-                if (magicRule[0] == extendedSkip2gram[2] &&
-                    magicRule[1] == extendedSkip2gram[3])
-                    parts[1] = layout.magicIdentifier;
-            });
-        }
+        let magicReplacements = 0;
+        if (layout.hasMagic)
+            for (let i = 0; i < layout.magicRules.length; i++) {
+                const replacement = extendedSkip2gram.replace(layout.magicRules[i], layout.magicRules[i][0] + layout.magicIdentifier);
+                if (replacement != extendedSkip2gram) {
+                    extendedSkip2gram = replacement;
+                    magicReplacements++;
+                    if (magicReplacements == 2)
+                        break;
+                }
+            }
         addGramAmount(parts.join(""), freq, skip2grams);
     }
     let skip2gramtotal = 0;
